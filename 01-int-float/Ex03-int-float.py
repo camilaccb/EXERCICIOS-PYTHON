40d06eb4e2f9a23aa504917709a7a10805f90fff
Camila <camisccb@gmail.com>
2023-07-29 22:15:13 -0300

    Correção do exercicio 3 de estrutura de dados int-float

--- 01-int-float/Ex03-int-float.py
+++ 01-int-float/Ex03-int-float.py
@@ -1,8 +1,8 @@
 # Faça um programa que receba os 3 lados de um triângulo e calcule sua área
 
 s1 = float(input('lado 1 em cm: ').replace(',','.'))
-s2 = float(input('lado 1 em cm: ').replace(',','.'))
-s3 = float(input('lado 1 em cm: ').replace(',','.'))
+s2 = float(input('lado 2 em cm: ').replace(',','.'))
+s3 = float(input('lado 3 em cm: ').replace(',','.'))
 
 # area = (s(s-s1)(s-s2)(s-s3))^(1/2)
 # s = (s1+s2+s3)/2
